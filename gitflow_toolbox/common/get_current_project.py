678d720b6c1080587563669059a15e30a10c8f3d
leorolland <rolland.leo@orange.fr>
2022-07-26 16:56:24 +0200

    fix: env var GITLAB_CI is not set

--- gitflow_toolbox/common/get_current_project.py
+++ gitflow_toolbox/common/get_current_project.py
@@ -1,3 +1,5 @@
+import os
+
 from gitlab.v4.objects.projects import Project
 
 from gitflow_toolbox.common.get_current_gitlab import get_current_gitlab
@@ -12,7 +14,7 @@ def get_current_project() -> Project:
     """
     gitlab = get_current_gitlab()
     # If running in a gitlab CI job
-    if get_env("GITLAB_CI"):
+    if os.getenv("GITLAB_CI"):
         # Use provided CI project, if not available, use project from current job
         project_id = get_env("GITLAB_PROJECT_ID", "CI_PROJECT_ID")
         return gitlab.projects.get(project_id)
