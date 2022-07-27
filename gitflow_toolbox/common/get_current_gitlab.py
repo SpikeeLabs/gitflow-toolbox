49a2d72b177e7fef653cc3a2a9dd32306aad5782
Léo <rolland.leo@orange.fr>
2022-07-27 09:29:00 +0200

    fix(#6): env var GITLAB_CI is not set

--- gitflow_toolbox/common/get_current_gitlab.py
+++ gitflow_toolbox/common/get_current_gitlab.py
@@ -1,3 +1,5 @@
+import os
+
 import gitlab
 
 from gitflow_toolbox.common.get_env import get_env
@@ -10,7 +12,7 @@ def get_current_gitlab() -> gitlab.Gitlab:
         gitlab.Gitlab: current gitlab instance (or 'source', 'from' gitlab)
     """
     # If running in a gitlab CI job
-    if get_env("GITLAB_CI"):
+    if os.getenv("GITLAB_CI"):
         # Use provided token, if not available, use default token from job
         private_token = get_env("GITLAB_PRIVATE_TOKEN", "CI_JOB_TOKEN")
         return gitlab.Gitlab(get_env("CI_SERVER_URL"), private_token)
