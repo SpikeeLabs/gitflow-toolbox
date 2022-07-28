75666bbfacdbb5b46e9086b8f983d02df8ff942c
Léo <rolland.leo@orange.fr>
2022-07-28 12:07:43 +0200

    feat(push): #9 - add push and get_repo_ssh_url

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from gitflow_toolbox.check_branch_exists import check_branch_exists
 from gitflow_toolbox.check_mr_exists import check_mr_exists
 from gitflow_toolbox.ensure_branch import ensure_branch
 from gitflow_toolbox.ensure_mr import ensure_mr
+from gitflow_toolbox.get_repo_ssh_url import get_project_ssh_url
+from gitflow_toolbox.push import push
 
 load_dotenv()
 
@@ -18,6 +20,8 @@ cli.add_command(check_branch_exists)
 cli.add_command(ensure_branch)
 cli.add_command(check_mr_exists)
 cli.add_command(ensure_mr)
+cli.add_command(get_project_ssh_url)
+cli.add_command(push)
 
 if __name__ == "__main__":
     cli()
