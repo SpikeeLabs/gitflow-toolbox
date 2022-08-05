8d39775d437aa61e2d836b455dae755922047fb4
Léo <rolland.leo@orange.fr>
2022-08-05 18:27:07 +0200

    feat(mr,diff): #13 - add diff and get_latest_mr_state

--- gitflow_toolbox/check_mr_exists.py
+++ gitflow_toolbox/check_mr_exists.py
@@ -19,6 +19,9 @@ def check_mr_exists(ctx: click.Context, remote: bool, source_branch: str, target
         remote (bool): whether to check on the current gitlab or remote gitlab (True=remote)
         source_branch (str): branch to create
         remote_target_branch (str): branch reference to create branch from
+
+    Returns:
+        bool: True if a MR exists (exit code 0), False otherwise (exit code 1)
     """
     click.echo(f"Checking if an opened merge request from {source_branch} to {target_branch} exists...")
     project = RemoteGitlab().project if remote else CurrentGitlab().project
